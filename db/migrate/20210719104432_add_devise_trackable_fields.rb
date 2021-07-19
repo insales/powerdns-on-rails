68a4166c760d72510c336ac083fe1653296b1cd7
Markin Maxim <mak22223@ya.ru>
2021-07-19 15:15:43 +0300

    Исправления для продакшена

--- db/migrate/20210719104432_add_devise_trackable_fields.rb
+++ db/migrate/20210719104432_add_devise_trackable_fields.rb
@@ -1,4 +1,4 @@
-class AddDeviseTrackebleFields < ActiveRecord::Migration[6.1]
+class AddDeviseTrackableFields < ActiveRecord::Migration[6.1]
   def change
     change_table :users do |t|
       t.integer  :sign_in_count, default: 0, null: false
