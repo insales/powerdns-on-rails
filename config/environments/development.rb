d9c090121abf207e3ef98077225eb2c8544fbbd4
Vasily Fedoseyev <vasilyfedoseyev@gmail.com>
2021-12-19 18:24:03 +0300

    rails app:update to 7

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,15 +14,18 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Enable server timing
+  config.server_timing = true
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+  if Rails.root.join("tmp/caching-dev.txt").exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+      "Cache-Control" => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
@@ -54,28 +57,18 @@ Rails.application.configure do
   config.active_record.verbose_query_logs = true
   config.active_record.verbose_query_logs = false
 
-  # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
-  # number of complex assets.
-  config.assets.debug = true
-  config.assets.debug = false
-
-  config.assets.css_compressor = :sass
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
   config.assets.raise_runtime_errors = true
+  config.assets.css_compressor = :sass
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
-
-  # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
@@ -84,6 +77,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost' }
 
   # Raise exception on mass assignment protection for Active Record models
+  # from protected_attributes_continued
   config.active_record.mass_assignment_sanitizer = :strict
 end
 
